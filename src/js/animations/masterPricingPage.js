a865376e78864b8114625bb0bbb3c3fa244985fd
Krzysztof <kpiekarz1983@gmail.com>
2019-09-23 17:56:25 +0200

    Fixed pricing page animation

--- src/js/animations/masterPricingPage.js
+++ src/js/animations/masterPricingPage.js
@@ -2,6 +2,13 @@ import { TimelineMax } from 'gsap';
 import girl from './pricingPage/girl';
 import priceCard from './pricingPage/priceCard';
 
+console.log(window);
+
 // Master timeline
 const master = new TimelineMax({ delay: 0.5 });
-master.add(girl()).add(priceCard(), '-=0.5');
+
+if (window.innerWidth <= 768) {
+  master.add(priceCard());
+} else {
+  master.add(girl()).add(priceCard(), '-=0.5');
+}
